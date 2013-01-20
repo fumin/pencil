ad9b1a644cc703baae8a3de42c04cb6c4659896d
fumin <r98723077@ntu.edu.tw>
2013-01-21 01:29:33 +0800

    use random filename

--- download_to_all.rb
+++ download_to_all.rb
@@ -7,7 +7,8 @@ urls = File.read(urls_file).split("\n")
 
 urls.each do |url|
   extname = /(\.\w+$)/.match(url)[1]
-  File.open(dest_dir + '/' + urls.index(url).to_s + extname, 'wb') do |f|
+  random_str = rand(2**256).to_s(36).ljust(8,'a')[0..7]
+  File.open(dest_dir + '/' + random_str + extname, 'wb') do |f|
     f.write Net::HTTP.get(URI(url))
   end
   puts "#{url} OK"
