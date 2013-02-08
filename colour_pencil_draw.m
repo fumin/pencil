1634b38c22e5aaad62c90bf2f325899b12696233
fumin wang <awaw@nandalu.idv.tw>
2013-02-08 20:49:29 +0800

    Modify UI Functions, All Is Ok?!

--- colour_pencil_draw.m
+++ colour_pencil_draw.m
@@ -1,7 +1,7 @@
-function R = colour_pencil_draw(I)
+function R = colour_pencil_draw(I,line_len_divisor,line_thickness_divisor,lambda,texture_resize_ratio,texture_file_name, col_coeff1,col_coeff2,col_coeff3,pen_coeff1,pen_coeff2,pen_coeff3)
 % Usage: imshow(colour_pencil_draw(imread('img/sign.png')))
 Iruv = rgb2ycbcr(I);
-Ypencil = pencil_draw(Iruv(:,:,1));
+Ypencil = pencil_draw(Iruv(:,:,1),line_len_divisor,line_thickness_divisor,lambda,texture_resize_ratio,texture_file_name, col_coeff1,col_coeff2,col_coeff3,pen_coeff1,pen_coeff2,pen_coeff3);
 
 new_Iruv = Iruv;
 new_Iruv(:,:,1) = uint8(Ypencil*255);
